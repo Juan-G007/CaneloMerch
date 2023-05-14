decd8c427e245378f672acf2341a85a471a35696
Juan-G007 <133581420+Juan-G007@users.noreply.github.com>
2023-05-14 07:38:10 -0600

    Add files via upload

---
+++ Contacto.js
@@ -0,0 +1,11 @@
+document.getElementById("InicioA").addEventListener("click", function() {
+    Swal.fire({
+        title: 'Correo: info@canelostore.com Teléfono de contacto: 33 2492 7654',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+      })
+  });
---
+++ Mensaje.js
@@ -0,0 +1,8 @@
+document.getElementById("botton1").addEventListener("click", function() {
+    Swal.fire(
+        'Sus datos se han mandado.',
+        'Espere nuevas Indicaciones en el correo electronico proporcionado.',
+        'success'
+      );
+  });
+  
